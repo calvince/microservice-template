ab0626ccaf6380a85663ef9cb26fa11fb7feddc7
calvince <calmosh1421@gmail.com>
2023-01-18 22:54:52 +0300

    Add get ProductById method that returns ProductResponse

--- src/main/java/com/vexla/ProductService/controller/ProductController.java
+++ src/main/java/com/vexla/ProductService/controller/ProductController.java
@@ -1,14 +1,12 @@
 package com.vexla.ProductService.controller;
 
 import com.vexla.ProductService.model.ProductRequest;
+import com.vexla.ProductService.model.ProductResponse;
 import com.vexla.ProductService.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -29,4 +27,12 @@ public class ProductController {
     }
 
     //get product by id
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable("id") long productId) {
+         ProductResponse productResponse =
+                 productService.getProductById(productId);
+
+         return new ResponseEntity<>(productResponse,HttpStatus.OK);
+
+    }
 }
