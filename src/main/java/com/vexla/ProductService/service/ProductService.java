a97de52255533309e788410765c31f89f86a8af1
calvince <calmosh1421@gmail.com>
2023-01-18 22:56:19 +0300

    getProductByid service

--- src/main/java/com/vexla/ProductService/service/ProductService.java
+++ src/main/java/com/vexla/ProductService/service/ProductService.java
@@ -1,7 +1,9 @@
 package com.vexla.ProductService.service;
 
 import com.vexla.ProductService.model.ProductRequest;
+import com.vexla.ProductService.model.ProductResponse;
 
 public interface ProductService {
     long addProduct(ProductRequest productRequest);
+    ProductResponse getProductById(long productId);
 }
