155dfaef277034bd4bb8515cadf0cef41a999a0d
calvince <calmosh1421@gmail.com>
2023-01-18 22:55:37 +0300

    Add @AllArgsConstructor annotation

--- src/main/java/com/vexla/ProductService/model/ProductResponse.java
+++ src/main/java/com/vexla/ProductService/model/ProductResponse.java
@@ -1,10 +1,14 @@
 package com.vexla.ProductService.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponse {
     private String productName;
     private long productId;
