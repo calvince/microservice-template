9e84bcc4321fd397cef6ecaee109e4d07d33baf9
calvince <calmosh1421@gmail.com>
2023-01-18 22:57:44 +0300

    implement getProductByid service interface

--- src/main/java/com/vexla/ProductService/service/ProductServiceImpl.java
+++ src/main/java/com/vexla/ProductService/service/ProductServiceImpl.java
@@ -1,11 +1,17 @@
 package com.vexla.ProductService.service;
 
 import com.vexla.ProductService.entity.Product;
+import com.vexla.ProductService.exception.ProductServiceCustomException;
 import com.vexla.ProductService.model.ProductRequest;
+import com.vexla.ProductService.model.ProductResponse;
 import com.vexla.ProductService.repository.ProductRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.beans.BeanUtils.*;
 
 @Service
 @Log4j2
@@ -30,5 +36,18 @@ public class ProductServiceImpl implements ProductService{
         log.info("Product created");
 
         return product.getProductId();
+    }
+    @Override
+    public ProductResponse getProductById(long productId) {
+        log.info("Get the product for productId: {}", productId);
+        //query Db
+        Product product = productRepository.findById(productId)
+                .orElseThrow(
+                        () -> new ProductServiceCustomException("Product with given id not found", "PRODUCT_NOT_FOUND"));
+
+        ProductResponse productResponse = new ProductResponse();
+        copyProperties(product, productResponse);
+
+        return productResponse;
     }
 }
